402aa6c132fdebb8c3229cd7d7d6cd8fd02e27e1
awtnb <arikaebanataw@gmail.com>
2025-08-30 20:20:39 +0900

    directly insert emoji

--- main.ts
+++ main.ts
@@ -64,7 +64,7 @@ export default class Yonda extends Plugin {
 			name: "リスト内に:bulb:",
 			icon: "lightbulb",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				addListLineWithSymbol(":bulb:", editor, view);
+				addListLineWithSymbol("\ud83d\udca1", editor, view);
 			},
 		});
 
